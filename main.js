3eb5a0b5ffa8984a16e700181c74e52df7eb8b24
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 06:11:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-
 onload = () =>{
-        const c = setTimeout(() => {
         document.body.classList.remove("container");
-</head>
 <body>        
     <div class="description">
         <span>We Love You Bu Nurhana</span>
