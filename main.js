c5e63d0b42c888cfef4ab25ed8d58ca8411c7128
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 06:24:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-
 onload = () => {
     document.body.classList.remove("container");
+     const titles = ('I LOVE U').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
 
-</head>
-<body>
-     <div class="description">
-        <span>From CGP Angk.10 Kab.Luwu Utara</span>
-    </div>
-</body>
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
 };
