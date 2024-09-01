4b1e15e4920c29d394e5e0d9af1d4c3f9e39fa4c
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 06:33:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 onload = () => {
     document.body.classList.remove("container");
+};
+</head>
+<body>
     <div class="description">
         <span>From CGP Angk.10 Kab.Luwu Utara</span>
-};
+</div>
+</body>
+</html>
