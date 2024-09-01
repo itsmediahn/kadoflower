841737bd95c4a88a7af22f10df7124a3eaeba77c
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 07:07:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
 onload = () => {
     document.body.classList.remove("container");
 };
-
-<!DOCTYPE html>
-<html lang="en">
-</head>
-<body>
-    <div class="greetings">
-    <span>Bu Nurhana</span>
-</div>
-</body>
-</html>
