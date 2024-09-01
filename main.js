097cb76640a2ba33640fcebb1c8fe1e482680aec
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 06:27:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
 onload = () => {
     document.body.classList.remove("container");
      const titles = ('I LOVE U').split('')
-    const titleElement = document.getElementById('title');
-    let index = 0;
-
-    function appendTitle() {
-      if (index < titles.length) {
-        titleElement.innerHTML += titles[index];
-        index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
-      }
-    }
-
-    appendTitle();
-
-    clearTimeout(c);
-  }, 1000);
 };
