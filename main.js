3daa0ed9179ec2c32bad655f22dcac660f4927ab
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 06:07:58 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
 
 onload = () =>{
         const c = setTimeout(() => {
-        document.body.classList.remove("not-loaded");
+        document.body.classList.remove("container");
         
-    const titles = ('We Love You Bu Nurhana').split('')
-    const titleElement = document.getElementById('title');
-    let index = 0;
-
-    function appendTitle() {
-      if (index < titles.length) {
-        titleElement.innerHTML += titles[index];
-        index++;
-        setTimeout(appendTitle, 300); // 1000ms delay
-      }
-    }
-
-    appendTitle();
-
-    clearTimeout(c);
-  }, 1000);
+    <div class="description">
+        <span>We Love You Bu Nurhana</span>
 };
