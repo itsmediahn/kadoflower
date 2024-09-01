eeffe2d9e8033df6bbce97ac9b15883168bcd023
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 05:44:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 onload = () =>{
         document.body.classList.remove("container");
+        
+    const titles = ('We Love You Bu Nurhana').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
 };
