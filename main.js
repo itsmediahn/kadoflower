7e1b44347fb98ea36a8b1bea788f6ff6b7aa04ff
itsmediahn <hardiantiarfah@gmail.com>
2024-09-01 06:49:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
- <head>
-  <title>Default Width dan Height di Tag Marquee</title>
- </head>
- <body>
-  <marquee>We Love You Bu Nurhana</marquee>
- </body>
-</html>
 onload = () => {
     document.body.classList.remove("container");
 };
